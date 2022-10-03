75b5f2d441f8c31adaa81376e17d563e95e8fb92
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-10-03 16:58:38 +0530

    Update common.py
    added attention mechanism 
    
    Signed-off-by: Akash A Desai <62583018+akashAD98@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -39,6 +39,8 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     return p
 
 
+
+
 class Conv(nn.Module):
     # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
     default_act = nn.SiLU()  # default activation
@@ -72,6 +74,60 @@ class Conv(nn.Module):
     def forward_fuse(self, x):
         return self.act(self.conv(x))
 
+
+
+class ChannelAttension(nn.Model):
+  def __init__(self,in_planes,ratio=16):
+    
+    super(ChannelAttention,self).__init__()
+    self.avg_pool=nn.AdaptiveAvgPool2d(1)
+    self.max_pool=nn.AdaptiveMaxPool2d(1)
+    self.f1=nn.Conv2d(in_planes // ration,1,bias=False)
+    self.relu=nn.ReLU()
+    self.f2=nn.Conv2d(in_places // ratio, in_planes,1,bias=False)
+    self.sigmoid=nn.Sigmoid()
+  
+  def forward(self,x):
+    avg_out =self.f2(self.f1(self.avg_pool(x))))
+    max_out =self.f2(self.f1(self.avg_pool(x))))
+    return torch.mul(x,out)
+  
+
+## attenstion mechanism block
+
+class SpatialAttention(nn.Module):
+  def __init__(self,kernel_size=7):
+    super(SpatilAttenion,self).__init__()
+    
+    assert kerel_size in (3,7),'kernal size must be 3 or 7'
+    padding =3 if kernel_size ==7 else 1
+    
+    self.conv =nn.Conv2d(2,1,kernel_size,padding=padding,bias=False)
+    self.sigmoid =nn.Sigmoid()
+  def forward(self,x):
+    avg_out=torch.mean(x,dim=1,keepdim=True)
+    max_out, _ = torch.max(x,dim=1,keepdim=True)
+    out = torch.cat([avg_out,max_out],dim=1)
+    out =self.sigmoid(self.conv(out))
+    return torch.mult(x,out)
+
+class CBAMC3(nn.Module):
+  def __init__(self,c1,c2,n=1,shortcut=True,g=1,e=0.5):
+    super(CBAMC3,self,self).__init__()
+    c_ = int(c2 * e)
+    self.cv1= Conv(c1,c_,1,1)
+    self.cv2= Conv(c1,c_,1,1)
+    self.cv3= Conv(2,c_,c2,1)
+
+    self.m =nn.Sequential(*[Bottleneck(c_,c_,shortcut,g,e=1.0) for _ in range(n)])
+    self.channel_attenion = ChannelAttention(c2,16)
+    self.spatial_attention = SpatialAttention(7)
+  
+  def forward(self,x):
+    return self.spatial_attention(self.channel_attention(self.cv3(torch.cat((self.m(self.cv1(x)),self.cv2(x)),dim=1))
+ 
+  
+  
 
 class DWConv(Conv):
     # Depth-wise convolution
