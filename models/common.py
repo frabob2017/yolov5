eb8f7d644e3b13b087aabe26e092af6f9625b4fb
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-10-16 09:11:06 +0530

    Update common.py
    removed few attention
    
    Signed-off-by: Akash A Desai <62583018+akashAD98@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -850,30 +850,6 @@ class Classify(nn.Module):
 
 ### attension mechanism
 
-
-# SE
-class SE(nn.Module):
-
-    def __init__(self, c1, c2, ratio=16):
-        super().__init__()
-        # c*1*1
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.l1 = nn.Linear(c1, c1 // ratio, bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.l2 = nn.Linear(c1 // ratio, c1, bias=False)
-        self.sig = nn.Sigmoid()
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avgpool(x).view(b, c)
-        y = self.l1(y)
-        y = self.relu(y)
-        y = self.l2(y)
-        y = self.sig(y)
-        y = y.view(b, c, 1, 1)
-        return x * y.expand_as(x)
-
-
 # CBAM
 class ChannelAttention(nn.Module):
 
@@ -958,61 +934,6 @@ class ECA(nn.Module):
         y = self.sigmoid(y)
 
         return x * y.expand_as(x)
-
-
-# CA
-class h_sigmoid(nn.Module):
-
-    def __init__(self, inplace=True):
-        super().__init__()
-        self.relu = nn.ReLU6(inplace=inplace)
-
-    def forward(self, x):
-        return self.relu(x + 3) / 6
-
-
-class h_swish(nn.Module):
-
-    def __init__(self, inplace=True):
-        super().__init__()
-        self.sigmoid = h_sigmoid(inplace=inplace)
-
-    def forward(self, x):
-        return x * self.sigmoid(x)
-
-
-class CoordAtt(nn.Module):
-
-    def __init__(self, inp, oup, reduction=32):
-        super().__init__()
-        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
-        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
-        mip = max(8, inp // reduction)
-        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(mip)
-        self.act = h_swish()
-        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        identity = x
-        n, c, h, w = x.size()
-        # c*1*W
-        x_h = self.pool_h(x)
-        # c*H*1
-        # C*1*h
-        x_w = self.pool_w(x).permute(0, 1, 3, 2)
-        y = torch.cat([x_h, x_w], dim=2)
-        # C*1*(h+w)
-        y = self.conv1(y)
-        y = self.bn1(y)
-        y = self.act(y)
-        x_h, x_w = torch.split(y, [h, w], dim=2)
-        x_w = x_w.permute(0, 1, 3, 2)
-        a_h = self.conv_h(x_h).sigmoid()
-        a_w = self.conv_w(x_w).sigmoid()
-        out = identity * a_w * a_h
-        return out
 
 
 # C3-SE
